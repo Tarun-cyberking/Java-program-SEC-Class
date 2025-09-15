8d854e1f15fa8c5fb4edfdbe2372d40dc8c5eef6
Tarun karthik M <tarunkarthik.manivasagam@gmail.com>
2025-09-15 19:21:28 +0530

    Add files via upload

---
+++ GradeExample.java
@@ -0,0 +1,13 @@
+public class GradeExample {
+    public static void main(String[] args) {
+        int marks = 45;
+
+        
+        String grade = (marks >= 90) ? "A" :
+                       (marks >= 75) ? "B" :
+                       (marks >= 50) ? "C":"F"; 
+
+        
+        System.out.println("Grade: " + grade);
+    }
+}
