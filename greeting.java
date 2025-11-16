72dff53bd64edfabe0fb17e69644dbe79f0ea79e
Tarun karthik M <tarunkarthik.manivasagam@gmail.com>
2025-11-16 11:22:46 +0530

    Add files via upload

---
+++ greeting.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class greeting {
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter your name");
+     String name=sc.nextLine();
+     System.out.println("Enter your age");
+     int age=sc.nextInt();
+     System.out.println("hello my name is - "+name+" and my age is -"+age);
+
+
+     
+    }
+    
+}
